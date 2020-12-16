d2b72d66a275d05a570ba0644bbbc3713c352122
Yarhoslav Munoz <ymuoz@uninorte.edu.co>
2020-12-16 17:14:11 -0500

    App structure.  Creating routes

--- app.py
+++ app.py
@@ -1,6 +1,35 @@
 from flask import Flask
-app = Flask(__name__)
+from flask import render_template, url_for
+
+app = Flask(__name__, template_folder="front-end")
 
 @app.route("/")
 def home():
-    return "Hello, Flask!"
+    return render_template('menu-general.html')
+
+@app.route("/login")
+def home1():    
+    return "Hola mundo!"
+
+@app.route("/crearproducto")
+def crearproducto():
+    return "Hola mundo!"
+
+@app.route("/modificarproducto")
+def modificarproducto():
+    return "Hola mundo!"
+
+@app.route("/eliminarproducto")
+def eliminarproducto():
+    return "Hola mundo!"
+
+@app.route("/registrarusuario")
+def registrarusuario():
+    return "Hola mundo!"
+
+@app.route("/actualizarinventario")
+def actualizarinventario():
+    return "Hola mundo!"
+
+if __name__ == "__main__":
+    app.run(debug=True)
